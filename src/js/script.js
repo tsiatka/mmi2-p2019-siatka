5f73090eb132286215b028c6bb47df0318e28cad
SIATKA TOM <tsiatka@iut.local>
2019-03-22 15:57:27 +0100

    edit section rules

--- src/js/script.js
+++ src/js/script.js
@@ -18,16 +18,11 @@ showSlides();
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("slideshow__slide");
-  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
+    slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
-     for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000);
 } 
